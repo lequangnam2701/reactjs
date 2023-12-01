74ee58891dd035418bc395613356a9062db300d2
lequangnam2701 <142825327+lequangnam2701@users.noreply.github.com>
2023-12-01 10:10:53 +0700

    Add files via upload

---
+++ baikiemtra2/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Danhsachhocsinh from './component/dadtastudents';
+
+
+ function MyApp () {
+  return (
+    <div className='App'>
+     <Danhsachhocsinh /> 
+    </div>
+  );
+}
+export default Danhsachhocsinh;
+
